aa8a8486a9d779691d3bc2fe5830bff7d5ae4b04
Konstantin <shirshikk@gmail.com>
2025-04-06 14:29:21 +0500

    add def new_product, getter and setter for price

--- src/products.py
+++ src/products.py
@@ -1,11 +1,30 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_of_product):
+        name = dict_of_product["name"]
+        description = dict_of_product["description"]
+        quantity = dict_of_product["quantity"]
+        price = dict_of_product["price"]
+        return Product(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
